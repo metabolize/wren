2d8947c1053a15cc006fd0051eee88a4af45a73d
Paul Melnikow <email@paulmelnikow.com>
2014-10-04 14:53:55 -0400

    Make installable with pip

---
+++ setup.py
@@ -0,0 +1,38 @@
+version = '0.1'
+
+# https://coderwall.com/p/qawuyq
+# Thanks James.
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   long_description = ''
+
+with open('requirements.txt', 'r') as f:
+    install_requires = [x.strip() for x in f.readlines()]
+
+from setuptools import setup
+
+setup(
+    name='wren',
+    version=version,
+    author='Body Labs',
+    author_email='paul.melnikow@bodylabs.com',
+    description='Synchronous RESTful API consumer based on Requests',
+    long_description=long_description,
+    url='https://github.com/bodylabs/wren',
+    license='Apache 2',
+    packages=[
+        'wren',
+    ],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
